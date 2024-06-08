e1b697686c7f82392d9f00ada474473319792488
Xanderistme <xanderitsme@gmail.com>
2024-06-07 20:52:08 -0500

    Improve styles using tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,8 +8,21 @@ export default {
 			colors: {
 				'icon-skyblue': 'rgb(30, 156, 241)',
 				'icon-lightGreen': 'rgb(0, 184, 122)',
-				'icon-carmesi': 'rgb(249, 26, 130)'
-			}
+				'icon-carmesi': 'rgb(249, 26, 130)',
+				'portage': {
+					'50': '#eff5fe',
+					'100': '#e3ecfc',
+					'200': '#cbdcfa',
+					'300': '#acc4f5',
+					'400': '#8ba3ee',
+					'500': '#7d8fe8',
+					'600': '#5360d8',
+					'700': '#444dbe',
+					'800': '#39419a',
+					'900': '#353d7a',
+					'950': '#1f2347',
+				},
+			},
 		}
 	},
 	plugins: [animations],

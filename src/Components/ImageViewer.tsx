ddec3c9661c517eeb383f25eedf9aa0338da62d4
Xanderistme <xanderitsme@gmail.com>
2024-06-10 02:31:33 -0500

    Feature: Add ImageViewer component

---
+++ src/Components/ImageViewer.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const ImageViewer = () => {
+  const [imgUrl, setImgUrl] = useState<string | null>(null)
+  const [classList, setClassList] = useState<string | null>('animate-fade-in')
+
+  useEffect(() => {
+    const images = document.querySelectorAll(".image-container");
+
+    images.forEach(($imageContainer) => {
+      $imageContainer.addEventListener("click", (event) => {
+        const $img = event.target?.closest("img");
+
+        if ($img instanceof HTMLImageElement) {
+          const newImgUrl = $img.getAttribute("src");
+          setClassList('animate-fade-in')
+          setImgUrl(newImgUrl)
+        }
+      });
+    });
+  }, [])
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.target !== event.target?.closest("img")) {
+      setClassList('animate-fade-out')
+      setTimeout(() => {
+        setImgUrl(null)
+      }, 200);
+
+    }
+  }
+
+  return (
+    imgUrl !== null &&
+    <div
+      className={`fixed w-dvw h-dvh bg-black/50 backdrop-blur top-0 animate-duration-faster ${classList}`}
+      data-id="img-viewer"
+      onClick={handleClick}
+    >
+      <picture className="w-full h-full flex items-center justify-center">
+        <img
+          className="object-contain max-h-[95dvh] max-w-[95dvw]"
+          src={imgUrl}
+          alt="Anything" />
+      </picture>
+    </div>
+  )
+}
--- src/utils/services.ts
+++ src/utils/services.ts
@@ -31,32 +31,43 @@ const posts = [
     likesCount: 6612,
     analiticsCount: 122461,
     contentPost:
-      'This is a simple post <a class="post-link" href="/">#Go to home</a>',
+      'This is a simple post <a class="post-link" href="#">#Go to home</a>',
   },
   {
-    username: "ひろ@C103/1日目東モ08a",
-    accountName: "habanero_mgmg",
-    avatarUrl: "/img/user1.webp",
+    username: "Reia🍀",
+    accountName: "Reia_76",
+    avatarUrl: "/img/user4.webp",
     postImgUrl: "/img/post4.webp",
-    repliesCount: 18,
-    repostsCount: 4219,
-    likesCount: 29415,
-    analiticsCount: 368193,
-    contentPost: "ヴェールヌイ",
+    repliesCount: 12,
+    repostsCount: 2512,
+    likesCount: 22175,
+    analiticsCount: 373193,
+    contentPost: '水着シロコ・セリカ <a class="post-link" href="#">#ブルアカ</a>',
   },
   {
     username: "日目東モ",
     accountName: "iorikawawa",
-    avatarUrl: "/img/user2.webp",
+    avatarUrl: "/img/user5.webp",
     postImgUrl: "/img/post5.webp",
-    repliesCount: 7,
-    repostsCount: 2217,
-    likesCount: 13612,
-    analiticsCount: 292461,
+    repliesCount: 9,
+    repostsCount: 1672,
+    likesCount: 19911,
+    analiticsCount: 275163,
     contentPost: "ヴェールヌイ",
+  },
+  {
+    username: "GongGo",
+    accountName: "Gonggo_901",
+    avatarUrl: "/img/user6.webp",
+    postImgUrl: "/img/post6.webp",
+    repliesCount: 16,
+    repostsCount: 2419,
+    likesCount: 22136,
+    analiticsCount: 256292,
+    contentPost: '<a class="post-link" href="#">#ブルアカ</a>'
   },
 ];
 
-export const fetchPosts = async (limit: number) => {
+export const fetchPosts = async (limit?: number) => {
   return posts.slice(0, limit)
 }

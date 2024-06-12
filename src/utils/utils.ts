dd580141038816293bcf9d1ffe9f212677882a36
Xanderistme <xanderitsme@gmail.com>
2024-06-12 17:09:19 -0500

    Fix: Remove unused variables and props

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export function debounce(func: Function, delay: number): Function {
-  let timeoutId: any;
-  return (...args) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
